6f96e946ccc6d6f5815d219ce10d9252b8008a10
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-16 03:02:32 +0000

    --

---
+++ H. suma simple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int numero, nuevoNumero;
+    
+    // cout << "Dame un numero: ";
+    cin >> numero;
+    
+    nuevoNumero = numero + 1;
+    
+    cout << nuevoNumero;
+    
+
+    return 0;
+}
