ce9dcb21c460dcb14261217714d03540ddffa0d3
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-19 23:52:59 +0000

    --

---
+++ 02 Estructuras de Control Condicionales/K. Estaciones.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int mes;
+  cin >> mes;
+  switch (mes){
+    case 3:
+    case 4:
+    case 5:
+      cout << "PRIMAVERA" << endl;
+      break;
+    case 6:
+    case 7:
+    case 8:
+      cout << "VERANO" << endl;
+      break;
+    case 9:
+    case 10:
+    case 11:
+      cout << "OTONIO" << endl;
+      break;
+    case 12:
+    case 1:
+    case 2:
+      cout << "INVIERNO" << endl;
+      break;
+  }
+
+  return 0;
+}
