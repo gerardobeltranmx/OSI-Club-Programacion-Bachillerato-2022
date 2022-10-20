97689678e57361eea29016738343c9c7ccac3424
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-20 00:23:12 +0000

    --

---
+++ 02 Estructuras de Control Condicionales/H. Menor y mayor de tres números.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c;
+  cin >> a  >> b >> c;
+  // Buscando menor
+  if (a < b and a < c){
+    cout << a << endl;
+  }
+  else if (b < a and b < c){
+    cout << b << endl;
+  }
+  else{
+    cout << c << endl;
+  }
+  // Buscando el mayor
+  if (a > b and a > c) {
+    cout << a << endl;
+  }   
+  else if (b > a and b > c){
+    cout << b << endl;
+  }
+  else{
+    cout << c << endl;
+  }
+    
+    
+    
+  
+  return 0;
+}
